5156e74ba06f3cfaad8ea69ba9c4764f6613464a
Guilhem Lettron <guilhem@lettron.fr>
2013-04-04 16:38:37 +0200

    My bad

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Guilhem Lettron"
 maintainer_email "guilhem.lettron@youscribe.com"
 name             "optipng"
 license          "Apache 2.0"
-description      "Install pgbadger"
+description      "Install optipng"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends          "apt"
